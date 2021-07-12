2f2283ba95a1a2ee276f3425c6b1ecb10b23e614
AliBakerSartawi <alisartawi@live.com>
2021-07-12 06:23:47 +0300

    models

---
+++ models/Post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    desc: { type: String, required: true, max: 1000 },
+    img: { type: String },
+    likes: { type: Array, default:[] },
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
